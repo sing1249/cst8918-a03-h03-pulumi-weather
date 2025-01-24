ea41128a000f93409ccd91513292b51434eb8ec6
sing1249 <sing1249@algonquinlive.com>
2025-01-24 18:19:53 -0500

    chore: add Docker image creation and Azure Container App deployment setup
    - Import @pulumi/docker module and define Docker image with build and registry configuration.
    - Add container group definition for Azure Container App with public ingress, resource limits, and environment variables.
    - Export IP address, hostname, and fully-qualified URL as outputs for testing.

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -1,6 +1,9 @@
 import * as pulumi from '@pulumi/pulumi'
 import * as resources from '@pulumi/azure-native/resources'
 import * as containerregistry from '@pulumi/azure-native/containerregistry'
+import * as docker from '@pulumi/docker'
+import * as containerinstance from '@pulumi/azure-native/containerinstance'
+
 // Import the configuration settings for the current stack.
 const config = new pulumi.Config()
 const appPath = config.require('appPath')
@@ -36,3 +39,81 @@ const registryCredentials = containerregistry
             password: creds.passwords![0].value!,
         }
     })
+
+// Define the container image for the service.
+const image = new docker.Image(`${prefixName}-image`, {
+    imageName: pulumi.interpolate`${registry.loginServer}/${imageName}:${imageTag}`,
+    build: {
+        context: appPath,
+        platform: 'linux/amd64',
+    },
+    registry: {
+        server: registry.loginServer,
+        username: registryCredentials.username,
+        password: registryCredentials.password,
+    },
+})
+
+
+// Create a container group in the Azure Container App service and make it publicly accessible.
+const containerGroup = new containerinstance.ContainerGroup(
+    `${prefixName}-container-group`,
+    {
+        resourceGroupName: resourceGroup.name,
+        osType: 'linux',
+        restartPolicy: 'always',
+        imageRegistryCredentials: [
+            {
+                server: registry.loginServer,
+                username: registryCredentials.username,
+                password: registryCredentials.password,
+            },
+        ],
+        containers: [
+            {
+                name: imageName,
+                image: image.imageName,
+                ports: [
+                    {
+                        port: containerPort,
+                        protocol: 'tcp',
+                    },
+                ],
+                environmentVariables: [
+                    {
+                        name: 'PORT',
+                        value: containerPort.toString(),
+                    },
+                    {
+                        name: 'WEATHER_API_KEY',
+                        value: 'b84e97f8ca6a20b05ffd232ba03b9358',
+                    },
+                ],
+                resources: {
+                    requests: {
+                        cpu: cpu,
+                        memoryInGB: memory,
+                    },
+                },
+            },
+        ],
+        ipAddress: {
+            type: containerinstance.ContainerGroupIpAddressType.Public,
+            dnsNameLabel: `${imageName}`,
+            ports: [
+                {
+                    port: publicPort,
+                    protocol: 'tcp',
+                },
+            ],
+        },
+    },
+)
+
+
+// Export the service's IP address, hostname, and fully-qualified URL.
+export const hostname = containerGroup.ipAddress.apply((addr) => addr!.fqdn!)
+export const ip = containerGroup.ipAddress.apply((addr) => addr!.ip!)
+export const url = containerGroup.ipAddress.apply(
+    (addr) => `http://${addr!.fqdn!}:${containerPort}`,
+)
